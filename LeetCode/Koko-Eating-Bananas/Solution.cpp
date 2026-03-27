1480d50afb40703f665a4d240003ff2229e5e4c3
Ashwani Thakur <bhumiharjiashwani@gmail.com>
2026-03-27 23:38:09 +0530

    Add solution for Koko Eating Bananas

--- LeetCode/Koko-Eating-Bananas/Solution.cpp
+++ LeetCode/Koko-Eating-Bananas/Solution.cpp
@@ -7,38 +7,39 @@
 7    for( int i = 0; i<piles.size(); i++){
 8        totalhourtakentoeat += ceil(piles[i]/(double)k);
 9    }
-10    if (totalhourtakentoeat <= h){return true;}
-11    else {return false;}
-12    // or 
-13    return totalhourtakentoeat <= h;
-14} 
-15
+10   /* if (totalhourtakentoeat <= h){return true;}
+11    else {return false;}  
+12    */
+13    // or 
+14    return totalhourtakentoeat <= h;
+15} 
 16
 17
-18    int minEatingSpeed(vector<int>& piles, int h) {
-19        int start = 1;
-20     /*   auto it = max_element(piles.begin(),pile.end());
-21        int end = *it ;    
-22    */  
-23        // or 
-24
-25        int end = * max_element(piles.begin(),piles.end());
-26        int ans = 0;
-27        while ( start <= end){
-28            int mid = start + (end - start)/2;
-29            int k = mid;
-30
-31            if (cankokofinishedbananawithkspeed(piles,h,k)){
-32                //if koko will finished all banana without being caught
-33                ans = k ;
-34                end = mid - 1;
-35            }
-36            else {
-37                start = mid + 1;
-38            }
-39            
-40        }
-41        return ans;
-42
-43    }
-44};
+18
+19    int minEatingSpeed(vector<int>& piles, int h) {
+20        int start = 1;
+21     /*   auto it = max_element(piles.begin(),pile.end());
+22        int end = *it ;    
+23    */  
+24        // or 
+25
+26        int end = * max_element(piles.begin(),piles.end());
+27        int ans = 0;
+28        while ( start <= end){
+29            int mid = start + (end - start)/2;
+30            int k = mid;
+31
+32            if (cankokofinishedbananawithkspeed(piles,h,k)){
+33                //if koko will finished all banana without being caught
+34                ans = k ;
+35                end = mid - 1;
+36            }
+37            else {
+38                start = mid + 1;
+39            }
+40            
+41        }
+42        return ans;
+43
+44    }
+45};
