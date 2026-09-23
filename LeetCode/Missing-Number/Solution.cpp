021c300714264dc72c80221f16a7eaed31650c0a
Ashwani Thakur <bhumiharjiashwani@gmail.com>
2026-09-23 19:38:37 +0530

    Add solution for Missing Number

--- LeetCode/Missing-Number/Solution.cpp
+++ LeetCode/Missing-Number/Solution.cpp
@@ -12,9 +12,21 @@
 12        //edge case 
 13        return nums.size();
 14    }
-15
-16    int missingNumber(vector<int>& nums) {
-17         return  sortingmethod(nums);
-18        
-19    }
-20};
+15    int xoremethod(vector<int>& nums){
+16        int ans = 0; 
+17        for(int i=0; i<nums.size(); i++){
+18            ans = ans^nums[i];
+19        }
+20        //second step 
+21        for(int i=0; i<=nums.size(); i++){
+22            ans = ans^i;
+23        }
+24        return ans;
+25    }
+26
+27    int missingNumber(vector<int>& nums) {
+28         //return  sortingmethod(nums);
+29         return xoremethod(nums);
+30        
+31    }
+32};
